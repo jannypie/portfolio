fe3b4540c39f95e05cebc95c95b08b8f9809857c
Jan Dennison <jldennison@gmail.com>
2015-02-18 16:01:25 -0500

    beginnings of dragon-game

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
+$('.card').on("click",function(){
 
+  if ($(this).hasClass('active')) {
+    $(this).removeClass('active');
+  } else {
+    var $this = $(this);
+    $('.card').removeClass('active');
+    $this.addClass('active');
+    // setTimeout(function(){
+    //   var elemTop = $this.offset().top;
+    //   window.scrollTo(0,elemTop - 20);
+    // },1000);
+  }
+});
